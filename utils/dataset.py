14f60b042e2147d9293f1744e71e9cd8a3179167
onng-wasabi <yuuto.onnga.23@gmail.com>
2018-08-14 00:57:36 +0900

    add load_fashion_mnist

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,23 +1,26 @@
+from pathlib import Path
+import gzip
 import pickle
 import numpy as np
 
-DATADIR = './data/cifar-10-batches-py/'
+DATADIR = Path(__file__).resolve().parents[1] / 'data'
 
 
 def load_cifar(kind=10):
+    cifar = DATADIR / 'cifar-10-batches-py'
     train_x = []
     train_y = []
     val_x = []
     val_y = []
     if kind == 10:
-        fnames = ['{}data_batch_{}'.format(DATADIR, i)
+        fnames = ['{}/data_batch_{}'.format(cifar, i)
                   for i in range(1, 6)]
         for fname in fnames:
             with open(fname, 'rb') as f:
                 dict = pickle.load(f, encoding='bytes')
             [train_x.append(datum) for datum in dict[b'data']]
             [train_y.append(label) for label in dict[b'labels']]
-        fname = '{}test_batch'.format(DATADIR)
+        fname = '{}/test_batch'.format(cifar)
         with open(fname, 'rb') as f:
             dict = pickle.load(f, encoding='bytes')
             [val_x.append(datum) for datum in dict[b'data']]
@@ -35,3 +38,22 @@ def load_cifar(kind=10):
     val_y = np.array(val_y)
 
     return train_x, train_y, val_x, val_y
+
+
+def read_gz(kind):
+    fashion = DATADIR / 'fashion-mnist'
+    with gzip.open('{}/{}-images-idx3-ubyte.gz'.format(fashion, kind), 'rb') as rf:
+        images = np.frombuffer(rf.read(), dtype=np.uint8, offset=16)
+    with gzip.open('{}/{}-labels-idx1-ubyte.gz'.format(fashion, kind), 'rb') as rf:
+        labels = np.frombuffer(rf.read(), dtype=np.uint8, offset=8)
+    return images, labels
+
+
+def load_fashion_mnist():
+    train_x, train_y = read_gz('train')
+    val_x, val_y = read_gz('t10k')
+    return train_x.reshape(-1, 1, 28, 28), train_y, val_x.reshape(-1, 1, 28, 28), val_y
+
+
+if __name__ == '__main__':
+    load_fashion_mnist()
